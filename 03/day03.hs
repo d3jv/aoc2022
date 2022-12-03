a9a96e6fb690add37fff9731237ff7aeac805f4c
David Štorek <xstorek@fi.muni.cz>
2022-12-03 15:49:16 +0100

    Implement day03 this is probably the last day in haskell

--- 03/day03.hs
+++ 03/day03.hs
@@ -1,26 +1,46 @@
+import System.IO
+import Data.Char
 import Data.List.Split
 import Data.List.Split.Internals
 import Data.List (sort)
 
 main = do handle <- openFile "input" ReadMode
           contents <- hGetContents handle
-          xs <- splitOn "\n" contents
           putStr "Task1: "
-          (putStr . task1 xs)
+          (putStr . show . task1) (splitOn "\n" contents)
+          putStr "\nTask2: "
+          (putStr . show . task2) (splitOn "\n" contents)
+          putStr "\n"
           return ()
+
+ord' x
+    | isLower x = ord x - 96
+    | isUpper x = ord x - 38
+    | otherwise = 0
+
+task2 :: [String] -> Int
+task2 = (sum . map (getCommon3 . map (sort . map ord')) . group3)
+    where
+        group3 :: [a] -> [[a]]
+        group3 [] = []
+        group3 xs = take 3 xs : group3 (drop 3 xs)
+
+        getCommon3 :: (Num a, Ord a) => [[a]] -> a
+        getCommon3 [xs, ys, zs] = head (intersectSorted xs (intersectSorted ys zs))
+        getCommon3 _ = 0
 
 halve :: [a] -> [[a]]
 halve xs = let len = (length xs) `div` 2 in [(take len xs), (drop len xs)]
 
-task1 :: [[a]] -> Int
-task1 = (sum . map intersectSorted . map ((map (sort . map ord) . halve)))
+task1 :: [String] -> Int
+task1 = (sum . map (getCommon . map (sort . map ord') . halve))
+    where
+        getCommon :: (Num a, Ord a) => [[a]] -> a
+        getCommon [xs, ys] = head (intersectSorted xs ys)
 
-getCommon :: [a] -> [a] -> a
-getCommon xs ys = (head . intersectSorted) xs ys
-
-intersectSorted :: Ord a => [a] -> [a] -> [a]
+intersectSorted :: (Num a, Ord a) => [a] -> [a] -> [a]
 intersectSorted (x:xs) (y:ys)
  | x == y    = x : intersectSorted xs ys
  | x < y     = intersectSorted xs (y:ys)
  | x > y     = intersectSorted (x:xs) ys
-intersectSorted _ _ = []
+intersectSorted _ _ = [0]
