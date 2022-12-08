271c54673f113078701445de9364a94daf4dc4e9
David Štorek <xstorek@fi.muni.cz>
2022-12-08 18:06:16 +0100

    Implement day08

--- 08/day08.hs
+++ 08/day08.hs
@@ -7,8 +7,9 @@ main = do handle <- openFile "input" ReadMode
           contents <- hGetContents handle
           putStr "Task1: "
           (putStr . show . task1) ((parse . splitOn "\n") contents)
+          --(putStr . show . parse . splitOn "\n") contents
           putStr "\nTask2: "
-          (putStr . show . task2) (splitOn "\n" contents)
+          (putStr . show . task2) ((parse . splitOn "\n") contents)
           putStr "\n"
           return ()
 
@@ -16,7 +17,7 @@ parse :: [String] -> [[Int]]
 parse = map (map digitToInt)
 
 isVisible :: Int -> Int -> [[Int]] -> Bool
-isVisible a b as = isVisibleCol a b as && isVisibleRow b (as !! a)
+isVisible a b as = isVisibleCol a b as || isVisibleRow b (as !! a)
     where
         isVisibleCol :: Int -> Int -> [[Int]] -> Bool
         isVisibleCol x i = (isVisibleRow x . map (!!i))
@@ -25,7 +26,7 @@ isVisible a b as = isVisibleCol a b as && isVisibleRow b (as !! a)
         isVisibleRow i xs = if i == 0 || i == length xs - 1 then True else 
                             let x = xs !! i 
                             in allSmaller x (take i xs)
-                            && allSmaller x (drop (i+1) xs)
+                            || allSmaller x (drop (i+1) xs)
 
         allSmaller :: Ord a => a -> [a] -> Bool
         allSmaller _ [] = True
@@ -43,6 +44,23 @@ task1 db = (sum . map (sum . map (\x -> bool 0 1 x)) . task 0) db
         row _ _ [] = []
         row x y (a:xs) = (isVisible x y db) : row x (y+1) xs
 
-task2 :: [String] -> Int
-task2 = undefined
+task2 :: [[Int]] -> Int
+task2 db = (maximum . map maximum . task 0) db
+    where
+        task _ [] = []
+        task x (a:xs) = frow x 0 a : task (x+1) xs
+
+        frow _ _ [] = []
+        frow x y (a:xs) = let row = db !! x
+                              col = map (!!y) db
+                              n = row !! y
+                         in
+                             (countVisible n ((reverse . take y) row) *
+                             countVisible n (drop (y+1) row) *
+                             countVisible n ((reverse . take x) col) *
+                             countVisible n (drop (x+1) col)) : (frow x (y+1) xs)
+
+        countVisible :: Int -> [Int] -> Int
+        countVisible _ [] = 0
+        countVisible n (x:xs) = if x >= n then 1 else 1 + countVisible n xs
 
