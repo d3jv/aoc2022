d44d26bc247967a301b699cf047b79ec717cff7c
David Štorek <xstorek@fi.muni.cz>
2022-12-08 17:21:32 +0100

    WIP: day08

--- 08/day08.hs
+++ 08/day08.hs
@@ -1,42 +1,47 @@
 import System.IO
 import Data.List.Split
 import Data.Char
+import Data.Bool
 
 main = do handle <- openFile "input" ReadMode
           contents <- hGetContents handle
           putStr "Task1: "
-          (putStr . show . task1) (splitOn "\n" contents)
+          (putStr . show . task1) ((parse . splitOn "\n") contents)
           putStr "\nTask2: "
           (putStr . show . task2) (splitOn "\n" contents)
           putStr "\n"
           return ()
 
-parse :: [String] -> [[(Int, Bool)]]
-parse = map (map (\x -> (x, False)) . map digitToInt)
+parse :: [String] -> [[Int]]
+parse = map (map digitToInt)
 
-isVisible :: Int -> Int -> [[(Int, Bool)]] -> Bool
-isVisible x y xs = isVisibleCol x y xs && isVisibleRow y (xs !! x)
+isVisible :: Int -> Int -> [[Int]] -> Bool
+isVisible a b as = isVisibleCol a b as && isVisibleRow b (as !! a)
     where
-        isVisibleCol :: Int -> Int -> [[(Int, Bool)]] -> Bool
+        isVisibleCol :: Int -> Int -> [[Int]] -> Bool
         isVisibleCol x i = (isVisibleRow x . map (!!i))
 
-        isVisibleRow :: Int -> [(Int, Bool)] -> Bool
-        isVisibleRow i xs = let ys = unPair xs
-                                x = ys !! i 
-                            in 
-                            allSmaller x (take i ys) && allSmaller x (drop (i+1) ys)
+        isVisibleRow :: Int -> [Int] -> Bool
+        isVisibleRow i xs = if i == 0 || i == length xs - 1 then True else 
+                            let x = xs !! i 
+                            in allSmaller x (take i xs)
+                            && allSmaller x (drop (i+1) xs)
 
         allSmaller :: Ord a => a -> [a] -> Bool
         allSmaller _ [] = True
-        allSmaller x = (and . map (<x))
+        allSmaller x xs = (and . map (<x)) xs
 
         unPair :: [(a, b)] -> [a]
         unPair = map (\(x, _) -> x)
 
-task1 :: [[(Int, Bool)]] -> Int
-task1 = 
+task1 :: [[Int]] -> Integer
+task1 db = (sum . map (sum . map (\x -> bool 0 1 x)) . task 0) db
     where
-        setVisible 
+        task _ [] = []
+        task x (a:xs) = row x 0 a : task (x+1) xs
+
+        row _ _ [] = []
+        row x y (a:xs) = (isVisible x y db) : row x (y+1) xs
 
 task2 :: [String] -> Int
 task2 = undefined
